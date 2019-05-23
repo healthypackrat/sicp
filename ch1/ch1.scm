0cf7039ba78f73bd916b437cb9fe9d36d052c17e
healthypackrat <healthypackrat@gmail.com>
2019-05-23 17:17:51 +0900

    Finish 1.1.7

--- ch1/ch1.scm
+++ ch1/ch1.scm
@@ -33,3 +33,28 @@
   (if (< x 0)
       (- x)
       x))
+
+; 1.1.7 例: Newton法による平方根
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+; (print (sqrt 9))
+; (print (sqrt (+ 100 37)))
+; (print (sqrt (+ (sqrt 2) (sqrt 3))))
+; (print (square (sqrt 1000)))
