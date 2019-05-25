bf9dd28ca7caa991102779fc06548384ac73d38d
healthypackrat <healthypackrat@gmail.com>
2019-05-25 19:37:56 +0900

    Finish 1.2.4

--- ch1/ch1.scm
+++ ch1/ch1.scm
@@ -128,3 +128,34 @@
         ((= kinds-of-coins 5) 50)))
 
 ; (print (count-change 100))
+
+; 1.2.4 べき乗
+
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+; (print (expt 2 8))
+
+(define (expt b n)
+  (expt-iter b n 1))
+
+(define (expt-iter b counter product)
+  (if (= counter 0)
+      product
+      (expt-iter b
+                 (- counter 1)
+                 (* b product))))
+
+; (print (expt 2 8))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+; (print (expt 2 8))
