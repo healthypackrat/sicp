b49efc8f6e100d30b75e1d067c7c1afc9497b11e
healthypackrat <healthypackrat@gmail.com>
2019-05-26 18:07:34 +0900

    Finish 1.2.6

--- ch1/ch1.scm
+++ ch1/ch1.scm
@@ -169,3 +169,49 @@
 
 ; (print (gcd 16 28))
 ; (print (gcd 206 40))
+
+; 1.2.6 素数性のテスト
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n) ; 脚注44
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+; Fermatテスト
+
+(use srfi-27)
+
+(define (random n)
+  (random-source-randomize! default-random-source)
+  (random-integer n))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+          (remainder (* base (expmod base (- exp 1) m))
+                     m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+; (print (prime? 561)) ; 561は素数ではない
+; (print (fast-prime? 561 1)) ; しかし素数として判定される
