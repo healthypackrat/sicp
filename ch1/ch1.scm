1e582200adb3a5e7041bc2bbd2910a265326107c
healthypackrat <healthypackrat@gmail.com>
2019-05-28 22:53:53 +0900

    Finish 1.3.3

--- ch1/ch1.scm
+++ ch1/ch1.scm
@@ -278,6 +278,67 @@
      dx ; 長方形の幅
      ))
 
-(print (integral cube 0 1 0.01))
+; (print (integral cube 0 1 0.01))
 
-(print (integral cube 0 1 0.001))
+; (print (integral cube 0 1 0.001))
+
+; 1.3.3 一般的方法としての手続き
+
+; 区間二分法による方程式の零点の探索
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value)
+                 (search f neg-point midpoint))
+                ((negative? test-value)
+                 (search f midpoint pos-point))
+                (else midpoint))))))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+            (error "Values are not of opposite sign" a b)))))
+
+; (print (half-interval-method sin 2.0 4.0))
+; => 3.14111328125
+
+; (print (half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3)) 1.0 2.0))
+; => 1.89306640625
+
+; 関数の不動点の探索
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+; (print (fixed-point cos 1.0))
+; => 0.7390822985224023
+
+; (print (fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0))
+; => 1.2587315962971173
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
+
+; (print (sqrt 2))
+; => 1.4142135623746899
