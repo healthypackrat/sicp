974a4145e9f0e94d64b2c33f67fd1aaf4b94f9d8
healthypackrat <healthypackrat@gmail.com>
2019-05-30 20:11:54 +0900

    Finish 1.3.4

--- ch1/ch1.scm
+++ ch1/ch1.scm
@@ -342,3 +342,72 @@
 
 ; (print (sqrt 2))
 ; => 1.4142135623746899
+
+; 1.3.4 値として返される手続き
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+; (print ((average-damp square) 10))
+; => 55
+
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+               1.0))
+
+; (print (sqrt 2))
+; => 1.4142135623746899
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+               1.0))
+
+; (print (cube-root 1000))
+; => 10.000002544054729
+
+; Newton法
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define dx 0.00001)
+
+(define (cube x) (* x x x))
+
+; (print ((deriv cube) 5))
+; => 75.00014999664018
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt x)
+  (newtons-method (lambda (y) (- (square y) x))
+                  1.0))
+
+; (print (sqrt 2))
+; => 1.4142135623822438
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (/ x y))
+                            average-damp
+                            1.0))
+
+; (print (sqrt 2))
+; => 1.4142135623746899
+
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+                            newton-transform
+                            1.0))
+
+; (print (sqrt 2))
+; => 1.4142135623822438
