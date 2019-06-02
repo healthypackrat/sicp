bbd5ebe644cab60080cbd71ce448c8dc0c535871
healthypackrat <healthypackrat@gmail.com>
2019-06-02 23:13:32 +0900

    Finish 2.2.1 (リスト演算)

--- ch2/ch2.scm
+++ ch2/ch2.scm
@@ -56,3 +56,46 @@
 
 ; (print-rat (add-rat one-third one-third))
 ; 2/3
+
+; 2.2.1 並びの表現
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define squares (list 1 4 9 16 25))
+
+; (print (list-ref squares 3))
+; 16
+
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+
+(define odds (list 1 3 5 7))
+
+; (print (length odds))
+; 4
+
+(define (length items)
+  (define (length-iter a count)
+    (if (null? a)
+        count
+        (length-iter (cdr a) (+ 1 count))))
+  (length-iter items 0))
+
+; (print (length odds))
+; 4
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+
+; (print (append squares odds))
+; (1 4 9 16 25 1 3 5 7)
+
+; (print (append odds squares))
+; (1 3 5 7 1 4 9 16 25)
