1d20deadc414912a2675043e7e19c13cb6d68f10
healthypackrat <healthypackrat@gmail.com>
2019-06-03 19:45:41 +0900

    Finish 2.2.2

--- ch2/ch2.scm
+++ ch2/ch2.scm
@@ -131,3 +131,54 @@
 
 ; (print (scale-list (list 1 2 3 4 5) 10))
 ; (10 20 30 40 50)
+
+; 2.2.2 階層構造
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+(define x (cons (list 1 2) (list 3 4)))
+
+; (print (count-leaves '()))
+; 0
+
+; (print (count-leaves 1))
+; 1
+
+; (print (count-leaves '(1)))
+; 1
+
+; (print (count-leaves '((1))))
+; 1
+
+; (print (count-leaves x))
+; 4
+
+; (print (count-leaves (list x x)))
+; 8
+
+; 木の写像
+
+(define (scale-tree tree factor)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (* tree factor))
+        (else (cons (scale-tree (car tree) factor)
+                    (scale-tree (cdr tree) factor)))))
+
+; (print (scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7))
+;                    10))
+; (10 (20 (30 40) 50) (60 70))
+
+(define (scale-tree tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (scale-tree sub-tree factor)
+             (* sub-tree factor)))
+       tree))
+
+; (print (scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7))
+;                    10))
+; (10 (20 (30 40) 50) (60 70))
