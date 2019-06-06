bcb44b55fb44d92633c7c2bfce8dfa7f5232a1aa
healthypackrat <healthypackrat@gmail.com>
2019-06-06 20:05:26 +0900

    Finish 2.2.3 (写像の入れ子)

--- ch2/ch2.scm
+++ ch2/ch2.scm
@@ -273,3 +273,68 @@
 
 ; (print (product-of-squares-of-odd-elements (list 1 2 3 4 5)))
 ; 225
+
+; 写像の入れ子
+
+; (accumulate append
+;             '()
+;             (map (lambda (i)
+;                    (newline)
+;                    (map (lambda (j)
+;                           (display (list i j))
+;                           (display " ")
+;                           (list i j))
+;                         (enumerate-interval 1 (- i 1))))
+;                  (enumerate-interval 1 6)))
+; (2 1)
+; (3 1) (3 2)
+; (4 1) (4 2) (4 3)
+; (5 1) (5 2) (5 3) (5 4)
+; (6 1) (6 2) (6 3) (6 4) (6 5)
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(use math.prime)
+
+(define (prime-sum? pair)
+  (small-prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap
+                 (lambda (i)
+                   (map (lambda (j) (list i j))
+                        (enumerate-interval 1 (- i 1))))
+                 (enumerate-interval 1 n)))))
+
+; (print (prime-sum-pairs 6))
+; ((2 1 3) (3 2 5) (4 1 5) (4 3 7) (5 2 7) (6 1 7) (6 5 11))
+
+(define (permutations s)
+  (if (null? s)
+      (list '())
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
+
+; (print (permutations '()))
+; (())
+
+; (print (permutations '(1)))
+; ((1))
+
+; (print (permutations '(1 2)))
+; ((1 2) (2 1))
+
+; (print (permutations '(1 2 3)))
+; ((1 2 3) (1 3 2) (2 1 3) (2 3 1) (3 1 2) (3 2 1))
