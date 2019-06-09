aff9705e0529601802c2e42a668f705c6b5c1bd4
healthypackrat <healthypackrat@gmail.com>
2019-06-09 19:23:06 +0900

    Finish 2.3.3 (順序づけられないリストとしての集合〜順序づけられたリストとしての集合)

--- ch2/ch2.scm
+++ ch2/ch2.scm
@@ -428,3 +428,51 @@
 
 ; (print (deriv '(* (* x y) (+ x 3)) 'x))
 ; (+ (* x y) (* y (+ x 3)))
+
+; 2.3.3 例: 集合の表現
+
+; 順序づけられないリストとしての集合
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+; (print (intersection-set '(1 2 3) '(2 3 4)))
+; (2 3)
+
+; 順序づけられたリストとしての集合
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= x (car set)) #t)
+        ((< x (car set)) #f)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+
+; (print (intersection-set '(1 2 3) '(2 3 4)))
+; (2 3)
