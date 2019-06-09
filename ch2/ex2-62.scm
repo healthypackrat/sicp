9efe77616f7e4c819a3b38ecdf90f235af03ecfb
healthypackrat <healthypackrat@gmail.com>
2019-06-09 19:25:57 +0900

    Add ex2-62.scm

---
+++ ch2/ex2-62.scm
@@ -0,0 +1,27 @@
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+          (let ((x1 (car set1))
+                (x2 (car set2)))
+            (cond ((= x1 x2)
+                   (cons x1 (union-set (cdr set1) (cdr set2))))
+                  ((< x1 x2)
+                   (cons x1 (union-set (cdr set1) set2)))
+                  ((< x2 x1)
+                   (cons x2 (union-set set1 (cdr set2)))))))))
+
+(print (union-set '() '()))
+; ()
+
+(print (union-set '(1) '(1)))
+; (1)
+
+(print (union-set '(1) '(2)))
+; (1 2)
+
+(print (union-set '(2) '(1)))
+; (1 2)
+
+(print (union-set '(1 3 5) '(2 4 6)))
+; (1 2 3 4 5 6)
